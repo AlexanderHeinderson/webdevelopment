874fa8c779ed6742a9fbda81be95307b735ac40a
AlexanderHeinderson <alexander.heinderson@student.vives.be>
2024-03-28 14:05:55 +0100

    aanpassing colorpicker

--- Semester2/Labo_6/Opdracht Colorpicker_uitbreiding/scripts/code.js
+++ Semester2/Labo_6/Opdracht Colorpicker_uitbreiding/scripts/code.js
@@ -3,24 +3,23 @@ const setup = () => {
 	let btnSave = document.getElementById("btnSave");
 
 	for (let i = 0; i < sliders.length; i++) {
-		sliders[i].addEventListener("change", update);
-		sliders[i].addEventListener("input", update);
+		sliders[i].addEventListener("change", updateNotSaved);
+		sliders[i].addEventListener("input", updateNotSaved);
 	}
 
+	let valueRed = parseInt(sliders[0].value);
+	let valueGreen = parseInt(sliders[1].value);
+	let valueBlue = parseInt(sliders[2].value);
+
 	// maak het blokje rood
-	update();
+	update(valueRed, valueGreen, valueBlue);
 
 	btnSave.addEventListener("click", save);
 
 }
 
-const update = () => {
-	let colorDemos= document.getElementsByClassName("colorDemo");
-	let sliders = document.getElementsByClassName("slider");
-	let valueRed = parseInt(sliders[0].value);
-	let valueGreen = parseInt(sliders[1].value);
-	let valueBlue = parseInt(sliders[2].value);
-	let test = 10;
+const update = (valueRed, valueGreen, valueBlue) => {
+	let colorDemo= document.querySelector(".colorDemo");
 	let red = document.getElementById("red");
 	let green = document.getElementById("green");
 	let blue = document.getElementById("blue");
@@ -29,9 +28,23 @@ const update = () => {
 	green.innerText = valueGreen;
 	blue.innerText = valueBlue;
 
+	let sliders = document.getElementsByClassName("slider");
+	sliders[0].value = valueRed;
+	sliders[1].value = valueGreen;
+	sliders[2].value = valueBlue;
 
-	colorDemos[0].style.backgroundColor = "rgb(" + valueRed + "," + valueGreen + ", " + valueBlue + ")";
+	colorDemo.style.backgroundColor = "rgb(" + valueRed + "," + valueGreen + ", " + valueBlue + ")";
 }
+
+const updateNotSaved = () =>{
+	let colorDemos= document.getElementsByClassName("colorDemo");
+	let sliders = document.getElementsByClassName("slider");
+	let valueRed = parseInt(sliders[0].value);
+	let valueGreen = parseInt(sliders[1].value);
+	let valueBlue = parseInt(sliders[2].value);
+	update(valueRed, valueGreen, valueBlue);
+}
+
 
 const save = () =>{
 	let sliders = document.getElementsByClassName("slider");
@@ -46,31 +59,18 @@ const save = () =>{
 
 	saves.appendChild(savedColor);
 	let colorDemos = document.getElementsByClassName("colorDemo");
-	savedColor.setAttribute("data-positie", colorDemos.length-1);
 	colorDemos[colorDemos.length-1].style.backgroundColor = "rgb(" + valueRed + "," + valueGreen + ", " + valueBlue + ")";
 
-	savedColor.addEventListener("click", setColor.bind(null, "rgb(" + valueRed + "," + valueGreen + ", " + valueBlue + ")"));
+	savedColor.addEventListener("click", update.bind(null, valueRed, valueGreen, valueBlue));
 
 	let remove = document.createElement("input");
 	remove.setAttribute("type", "button");
 	remove.setAttribute("value", "X");
 	remove.setAttribute("class", "btnRemove");
-	remove.addEventListener("click", removeColor.bind(null, colorDemos.length-1));
-
+	remove.addEventListener("click", () => {
+		savedColor.remove();
+	});
 	savedColor.appendChild(remove);
-
-}
-
-const setColor = (code) =>{
-	let block = document.querySelector(".colorDemo");
-	block.style.backgroundColor = code;
-}
-
-
-const removeColor = (positie) =>{
-	let saves = document.getElementById("saves");
-	let toRemove = saves.childNodes;
-	toRemove[positie-1].classList.add("hidden");
 }
 
 window.addEventListener("load", setup);
